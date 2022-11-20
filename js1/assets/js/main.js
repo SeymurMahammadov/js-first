2c2dc7db00a5cc84d56f9973e5914d7bfd91b551
SeymurMahammadov <112489438+SeymurMahammadov@users.noreply.github.com>
2022-11-20 17:07:34 +0100

    last

--- js1/assets/js/main.js
+++ js1/assets/js/main.js
@@ -1,7 +1,35 @@
 
 //Task:Area of the circle
-//console.log (Math.PI*Math.pow(8,2));
+console.log (Math.PI*Math.pow(8,2));
 
 //Task:Time
-var date= new Date().setHours(6-12)
+var date= new Date;
+
+var time=date.getHours();
+console.log(time)
+
+if (time>6 && time<12) {
+
+    alert('Good morning')
+    document.body.style.backgroundColor= 'white'
+    
+} 
+
+if (time>12 && time<18) {
+
+    document.body.style.backgroundColor= 'orange'
+    alert('good afternoon')
+    
+    
+} 
+else {
+
+    alert('good evening')
+    document.body.style.backgroundColor= 'black'
+
+    
+}
+
+
+
 
